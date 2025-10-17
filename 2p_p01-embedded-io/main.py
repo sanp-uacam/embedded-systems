dc3b76f429bd24b7c36c27e550c09ae1cdb07a63
SERGIO NP <serganoh@uacam.mx>
2025-10-17 09:07:13 -0600

    main structure better

--- 2p_p01-embedded-io/main.py
+++ 2p_p01-embedded-io/main.py
@@ -1,34 +1,39 @@
 import sys
 import os
-
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import RPi.GPIO as GPIO
 import time
-
 from servo_control import set_servo_angle
 
 SERVO_PIN = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
-
 pwm = GPIO.PWM(SERVO_PIN, 50)
-pwm.start(0) 
 
-try:
-    print("Iniciando secuencia del servomotor. Presiona CTRL+C para detener.")
-    while True:
-        print("Moviendo a 90 grados...")
-        set_servo_angle(90, pwm)
-        time.sleep(3)
-        print("Moviendo a -90 grados...")
-        set_servo_angle(-90, pwm)
-        time.sleep(3)
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SERVO_PIN, GPIO.OUT)
 
-except KeyboardInterrupt:
-    print("Programa detenido por el usuario.")
+    pwm.start(0) 
 
-finally:
-    pwm.stop()          
-    GPIO.cleanup()      
-    print("GPIO limpiado y programa finalizado.")
+def loop():
+    print("Moviendo a 90 grados...")
+    set_servo_angle(90, pwm)
+    time.sleep(3)
+    print("Moviendo a -90 grados...")
+    set_servo_angle(-90, pwm)
+    time.sleep(3)
+    
+if __name__ == "__main__": 
+    setup()
+    
+    try:
+        while True:
+            loop()
+            
+    except KeyboardInterrupt:
+        print("Programa detenido por el usuario.")
+
+    finally:
+        pwm.stop()          
+        GPIO.cleanup()      
+        print("GPIO limpiado y programa finalizado.")
