de1db021675f927621e70668d6326ec8ada0ad4d
SERGIO NP <serganoh@uacam.mx>
2025-10-17 00:23:59 -0600

    new structure

---
+++ 2p_p01-embedded-io/main.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import RPi.GPIO as GPIO
+import time
+
+from servo_control import set_servo_angle
+
+SERVO_PIN = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+pwm = GPIO.PWM(SERVO_PIN, 50)
+pwm.start(0) 
+
+try:
+    print("Iniciando secuencia del servomotor. Presiona CTRL+C para detener.")
+    while True:
+        print("Moviendo a 90 grados...")
+        set_servo_angle(90)
+        time.sleep(3)
+        print("Moviendo a -90 grados...")
+        set_servo_angle(-90)
+        time.sleep(3)
+
+except KeyboardInterrupt:
+    print("Programa detenido por el usuario.")
+
+finally:
+    pwm.stop()          
+    GPIO.cleanup()      
+    print("GPIO limpiado y programa finalizado.")
---
+++ 2p_p01-embedded-io/servo_control.py
@@ -0,0 +1,25 @@
+
+def set_servo_angle(angle):
+    duty = 2.5 + (angle + 90) / 180 * 10
+    pwm.ChangeDutyCycle(duty)   
+    time.sleep(0.5) 
+    pwm.ChangeDutyCycle(0)
+
+
+# try:
+#     print("Iniciando secuencia del servomotor. Presiona CTRL+C para detener.")
+#     while True:
+#         print("Moviendo a 90 grados...")
+#         set_servo_angle(90)
+#         time.sleep(3)
+#         print("Moviendo a -90 grados...")
+#         set_servo_angle(-90)
+#         time.sleep(3)
+
+# except KeyboardInterrupt:
+#     print("Programa detenido por el usuario.")
+
+# finally:
+#     pwm.stop()          
+#     GPIO.cleanup()      
+#     print("GPIO limpiado y programa finalizado.")
